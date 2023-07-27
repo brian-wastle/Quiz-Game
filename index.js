9d19d5c628da24ae3988c98b7bf836aac6fccf12
brian-wastle <brian.wastle1@gmail.com>
2023-07-27 09:50:40 -0400

    added README.md and timer which renders below answer buttons

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 let quizDiv = document.querySelector("#quiz");
+let questionRender = document.querySelector("#question");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
+let timerRender = document.querySelector("#timer");
 
-let timer = 90;
 
-//let timerId = setInterval(function){
 
-//timer -=1;
+// let timer = 90;
 
-//}
+// let timerId = setInterval(function(){
+
+// timer -=1;
+// timerRender.textContent = timer;
+// }, 1000);
+
+
 
 let questions = [{question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+                 {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+                 {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
                  {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" }
 ]
 
 let currentQuestion = 0;
 
+
+
+
+
+
+
+
+
 renderQuestion();
 
 function renderQuestion() {
 
-    questions[currentQuestion].question;
+    questionRender.textContent = questions[currentQuestion].question;
     questionButton1.textContent = questions[currentQuestion].answers[0];
     questionButton2.textContent = questions[currentQuestion].answers[1];
     questionButton3.textContent = questions[currentQuestion].answers[2];

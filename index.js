df03c8cc47161d8c38da28e6544e7222f719aaca
brian-wastle <brian.wastle1@gmail.com>
2023-07-27 19:45:00 -0400

    finished timer function, game ends when questions do

--- index.js
+++ index.js
@@ -1,63 +1,111 @@
-let quizDiv = document.querySelector("#quiz");
 let questionRender = document.querySelector("#question");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
 let timerRender = document.querySelector("#timer");
+let startMenuId = document.getElementById("start-menu");
+let startButton = document.querySelector("#start-button");
+let quizButtons = document.querySelector("#quiz");
+let timerVis = document.querySelector("#timer");
+let currentQuestion = -1;
+let score = 0;
+let timerId = "";
+quizButtons.style.visibility = "hidden";
+timerVis.style.visibility = "hidden";
 
-
-
-// let timer = 90;
-
-// let timerId = setInterval(function(){
-
-// timer -=1;
-// timerRender.textContent = timer;
-// }, 1000);
-
-
-
-let questions = [{question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
-                 {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
-                 {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
-                 {question: "test", answers:["test1", "test2", "test3", "test4"], correctAnswer: "test5" }
+let questions = [{ question: "test q 1", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 2", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 3", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 4", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 5", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 6", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 7", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" },
+{ question: "test q 8", answers: ["test1", "test2", "test3", "test4"], correctAnswer: "test5" }
 ]
 
-let currentQuestion = 0;
+
+let timer = 91;
 
 
 
+// clear timer
+// 
 
 
 
+startButton.addEventListener("click", function () {
+    currentQuestion += 1;
+    console.log(currentQuestion);
+    startQuiz();
+    timerVis.style.visibility = "visible";
+    timerId = setInterval(function(){
+
+        timer -=1;
+        timerRender.textContent = timer;
+        }, 1000);
+    renderQuestion();
+    
+});
 
 
-
-renderQuestion();
-
-function renderQuestion() {
-
-    questionRender.textContent = questions[currentQuestion].question;
-    questionButton1.textContent = questions[currentQuestion].answers[0];
-    questionButton2.textContent = questions[currentQuestion].answers[1];
-    questionButton3.textContent = questions[currentQuestion].answers[2];
-    questionButton4.textContent = questions[currentQuestion].answers[3];
-    //show correct answer questions[currentQuestion].correctAnswer
-
-}
-
-quizDiv.addEventListener("click", function(event) {
+quizButtons.addEventListener("click", function (event) {
 
     if (event.target.matches("button")) {
 
         console.log("clicked!");
         console.log("value:" + event.target.innerText)
         console.log("correct answer: " + questions[currentQuestion].correctAnswer);
+        compareAnswers();
+
+        function compareAnswers() {
+            if (event.target.innerText == questions[currentQuestion].correctAnswer) {
+                score +=500;
+            }
+            else {
+                timer -= 5;
+            }
+
+        }
+
 
         currentQuestion++;
-        renderQuestion();
+        if (currentQuestion <= questions.length-1) {
+            console.log(currentQuestion);
+            console.log(questions.length)
+            renderQuestion();
+        } 
+        else {
+            timerVis.style.visibility = "hidden";
+            clearInterval(timerId);
+            endQuiz();
+        }
     }
 
 })
+
+
+
+function startQuiz() {
+    startMenuId.style.visibility = "hidden";
+    quizButtons.style.visibility = "visible";
+    
+}
+
+function endQuiz() {
+    quizButtons.style.visibility = "hidden";
+    
+}
+
+
+function renderQuestion() {
+    questionRender.textContent = questions[currentQuestion].question;
+    questionButton1.textContent = questions[currentQuestion].answers[0];
+    questionButton2.textContent = questions[currentQuestion].answers[1];
+    questionButton3.textContent = questions[currentQuestion].answers[2];
+    questionButton4.textContent = questions[currentQuestion].answers[3];
+    //show correct answer questions[currentQuestion].correctAnswer
+}
+
+
 
